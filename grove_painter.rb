11aeecfe7cfbfb11bca85b1d4afa9a9a9fdf311f
Katie <kab761@gmail.com>
2014-09-18 15:45:06 -0700

    fixed stupid brokenness

---
+++ grove_painter.rb
@@ -0,0 +1,88 @@
+
+  class GrovePainter
+
+    def initialize(grove)
+      @grove = grove
+      @soil_char = check_soil_quality
+      @tree_states = check_tree_states
+      draw
+    end
+
+    def check_soil_quality
+      if @grove.soil_quality < 0.25
+        @soil_char = "_"
+      elsif @grove.soil_quality < 0.75
+        @soil_char = "."
+      else
+        @soil_char = ","
+      end
+      return @soil_char
+    end
+
+    def check_tree_states
+      @tree_states = []
+
+      @grove.trees.each do |tree|
+        case tree.age_category
+        when :sapling
+          tree_graphic = "_:_"
+        when :young, :old
+          tree_graphic = "_l_"
+        when :mature
+          tree_graphic = "<|>"
+        when :dead
+          tree_graphic = "_!_"
+        else # this shouldn't happen
+          tree_graphic = "???"
+        end
+        @tree_states << {
+          count: tree.count_oranges.to_s.center(7),
+          graphic: tree_graphic.center(7, @soil_char)
+          }
+      end
+      return @tree_states
+    end
+
+    def draw
+      if @grove.size <= 8
+        rows = 1
+        last_row_size = @grove.size
+      elsif @grove.size % 8 ==0
+        rows = @grove.size / 8
+        last_row_size = 8
+      else
+        rows = @grove.size / 8 + 1
+        last_row_size = @grove.size % 8
+      end
+
+      draw_border
+      current_row = 0
+      (rows - 1).times do
+        draw_row(current_row)
+        current_row += 1
+      end
+
+      draw_row(current_row, last_row_size)
+      puts "\n\n"
+    end
+
+    def draw_border
+      puts "-" * 56
+      puts " " * 56
+    end
+
+    def draw_row(current_row, row_width=8)
+      index = current_row * 8
+      trees_this_row = @tree_states.values_at(index...index+row_width)
+      counts_this_row = trees_this_row.collect { |tree| tree[:count] }
+      graphics_this_row = trees_this_row.collect{ |tree| tree[:graphic] }
+
+      row_counts_string = counts_this_row.join
+      row_graphics_string = graphics_this_row.join
+
+    puts row_counts_string
+    puts row_graphics_string
+    draw_border
+    end
+
+  end
---
+++ orange_grove.rb
@@ -0,0 +1,91 @@
+require './broken_orange_tree.rb'
+require './broken_grove_painter.rb'
+
+class OrangeGrove
+
+  attr_accessor :trees, :soil_quality
+
+  def initialize(n, capacity=100)
+    @trees = []
+    @capacity = capacity
+    @soil_quality = 1
+
+    puts "Initial planting:"
+    plant_trees(n)
+  end
+
+  def to_s
+    return "The orange grove has #{size} trees out of a maximum of #{@capacity}, with soil quality #{@soil_quality * 100}%."
+  end
+
+  def size
+    return @trees.length
+  end
+
+  def plant_trees(n=1)
+    soil_damage = n.to_f / @capacity
+
+    if size == @capacity
+      n = 0
+      puts "No new trees planted (grove is at capacity):"
+    elsif size + n >= @capacity
+      n = @capacity - size
+      @soil_quality = 0
+      puts "After planting #{n} new trees (grove is now at capacity):"
+    else
+      @soil_quality -= soil_damage
+      puts "After planting #{n} new trees:"
+    end
+
+    n.times do
+      @trees << OrangeTree.new(0, 0, 0, @soil_quality)
+    end
+
+    puts "Planted #{n} trees, to total of #{size}"
+    if size%8==0
+      if size>8
+        puts "THIS WILL BREAK!!!"
+      else
+        puts "WILL THIS BREAK???"
+      end
+    end
+    GrovePainter.new(self)
+  end
+
+  def years_pass(n=1)
+    n.times do
+      @trees.each do |tree|
+        tree.one_year_passes(@soil_quality)
+      end
+    end
+    puts "After #{n} years"
+    GrovePainter.new(self)
+  end
+
+  def count_all_oranges
+    total_oranges = 0
+    @trees.each do |tree|
+      total_oranges += tree.count_oranges
+    end
+    return total_oranges
+  end
+end
+
+def main
+
+  trees = rand(1..16)
+  percent_capacity = rand(1..50) / 100.0
+  capacity = (trees / percent_capacity).round
+
+  puts "#{trees} trees out of max #{capacity}"
+  grove = OrangeGrove.new(trees, capacity)
+
+  4.times do
+    grove.years_pass(10)
+    grove.trees[0].pick_oranges
+    grove.plant_trees(rand(0..9))
+  end
+
+end
+
+100.times {main}
